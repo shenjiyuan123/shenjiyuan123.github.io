2c5f2a311779b953ffb4ba47fec8ec13f7c336dd
shenjiyuan123 <815462629@qq.com>
2022-07-07 15:16:31 +0800

    travel map

---
+++ _pages/life.md
@@ -0,0 +1,36 @@
+---
+layout: archive
+title: "Life"
+permalink: /life/
+author_profile: true
+---
+
+{% include base_path %}
+
+This is a collection of thoughts and advice on a wide range of topics, specifically about my daily life, updated whenever I think of something new. It is very much work-in-progress.
+
+## Sport
+
+
+
+### Competitions
+
+
+## Music
+
+
+## Travel Map
+
+This map gives an overview of the countries that I have visited (10 countries so far!):
+
+<html>
+  <link rel="stylesheet" href="/files/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
+  <script src="/files/jquery-3.4.1.min.js"></script>
+  <script src="/files/jquery-jvectormap-2.0.5.min.js"></script>
+  <script src="/files/jquery-jvectormap-world-mill.js"></script>
+
+  <div id="map" style="width: 600px; height: 400px"></div>
+
+<script type="text/javascript" src="/files/travel_map.js"></script>
+
+</html>
---
+++ files/travel_map.js
@@ -0,0 +1,48 @@
+;(function($) {
+
+    function initMap() {
+
+        $('#map').vectorMap({
+            map: 'world_mill',
+            backgroundColor: '#ffffff',
+            zoomMin:0.85,
+            regionStyle:{
+                initial: {
+                    fill: '#aaaaaa',
+                    "fill-opacity": 1,
+                    stroke: 'none',
+                    "stroke-width": 0,
+                    "stroke-opacity": 0
+                },
+                hover: {
+                    "fill-opacity": 0.8,
+                    cursor: 'pointer'
+                },
+                selected: {
+                    fill: 'yellow'
+                },
+                selectedHover: { }
+            },
+            series: {
+                regions: [{
+                  values: {
+                        CN:'#5688C7',
+                        RU:'#5688C7',
+                        US:'#5688C7',
+                        JP:'#5688C7',
+                        SE:'#5688C7',
+                        FN:'#5688C7',
+                        EE:'#5688C7',
+                        MY:'#5688C7',
+                        TH:'#5688C7',
+                        VN:'#5688C7',
+                        TW:'#5688C7',
+                  }
+                }]
+              }
+        });
+    }
+
+    initMap();
+    
+}) (jQuery);
